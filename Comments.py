2a22a22375b9d3f33173b3db84d5225afe941420
Bryan4578 <brivas@mchenry.edu>
2024-05-27 22:41:33 -0500

    Add files via upload

---
+++ Comments.py
@@ -0,0 +1,8 @@
+#The size of the tank
+tank_size = 15
+#The distance the tank went 
+distance = 600
+#Distance divided by the size of the Tank
+mpg = distance / tank_size
+#Miles per gallon 
+print("Miles per gallon:", mpg)
