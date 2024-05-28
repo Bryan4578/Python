a58e48ffe946268fc934c7778a817803fccd5fae
Bryan4578 <brivas@mchenry.edu>
2024-05-27 21:59:52 -0500

    Add files via upload

---
+++ Area of a rectangle.py
@@ -0,0 +1,8 @@
+#This is the area of a rectangle 
+length = 7
+width = 7
+area = length * width
+print("\nMultiplication: ")
+print(area)
+print("")
+print("The area of a rectangle with a length of 7 and a width of 7 is 49")
