616d2ae6cf657c5bd760d3418bab6aee04d180fc
Bryan4578 <brivas@mchenry.edu>
2024-05-27 22:28:40 -0500

    Add files via upload

---
+++ kg to lb.py
@@ -0,0 +1,14 @@
+#kg coverted to pounds 
+kg_value_1 = 37.8
+kg_value_2 = 72.14
+kg_value_3 = 855
+kg_value_4 = 14
+conversion_factor = 2.20462
+pounds_1 = kg_value_1 / conversion_factor
+pounds_2 = kg_value_2 / conversion_factor
+pounds_3 = kg_value_3 / conversion_factor
+pounds_4 = kg_value_4 / conversion_factor
+print(f"{kg_value_1} kilograms is equal to {pounds_1:.2f} pounds.")
+print(f"{kg_value_2} kilograms is equal to {pounds_2:.2f} pounds.")
+print(f"{kg_value_3} kilograms is equal to {pounds_3:.2f} pounds.")
+print(f"{kg_value_4} kilograms is equal to {pounds_4:.2f} pounds.")
