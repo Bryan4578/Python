2fee36a893db398d973f9b42eb1d98b4d19133d1
Bryan4578 <brivas@mchenry.edu>
2024-05-27 21:19:31 -0500

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,22 @@
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 14
+print("Literal:", x)
+
+# Integer
+y = -8
+print("Integer:", y)
+
+# Float
+z = 7.7
+print("Float:", z)
+
+# String
+name = "Bryan"
+print("String:", name)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
