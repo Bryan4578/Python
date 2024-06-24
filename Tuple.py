30028bfd9cf49b71cc5e7718cf49263796794df1
Bryan4578 <brivas@mchenry.edu>
2024-06-23 22:14:34 -0500

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,14 @@
+programming_classes = (
+    'Intro to Python',
+    'Advanced Python',
+    'Database Essentials',
+    'Web Development Basics',
+    'Data Structures in Python',
+    'Web Design Fundamentals'
+)
+def main():
+    for class_name in programming_classes:
+        print(class_name)
+    print("Number of classes:", len(programming_classes))
+if __name__ == "__main__":
+    main()
